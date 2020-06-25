9202257773cfef77dc717095b2aed15e26130d9c
zhanjun <17989678@qq.com>
2020-06-26 00:00:44 +0800

    首次提交测试

---
+++ xye-boot-war/src/main/java/com/xye/boot/xyebootwar/XyeBootWarApplication.java
@@ -0,0 +1,13 @@
+package com.xye.boot.xyebootwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyeBootWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XyeBootWarApplication.class, args);
+    }
+
+}
